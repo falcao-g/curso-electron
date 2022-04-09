9936cfe5f1b9988fb26ca30a0a02274655168143
Falcão <gabriel.p.falcao50@gmail.com>
2022-04-09 18:22:05 -0400

    Update renderer.js
    tempo é carregado dos arquivos quando o aplicativo é aberto e quando o curso é trocado

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,10 +1,19 @@
 const { ipcRenderer } = require('electron');
+const data = require('../../data');
 const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
 let botaoPlay = document.querySelector('.botao-play')
 let tempo = document.querySelector('.tempo')
 let curso = document.querySelector('.curso')
+
+window.onload = () => {
+
+    data.pegaDados(curso.textContent)
+        .then((dados) => {
+            tempo.textContent = dados.tempo;
+        })
+}
 
 linkSobre.addEventListener('click', function() {
     ipcRenderer.send('abrir-janela-sobre');
@@ -21,4 +30,12 @@ botaoPlay.addEventListener('click', function() {
         botaoPlay.src = 'img/stop-button.svg'
         play = true
     }
+})
+
+ipcRenderer.on('curso-trocado', (event, nome_curso) => {
+    curso.textContent = nome_curso
+    data.pegaDados(nome_curso)
+        .then((dados) => {
+            tempo.textContent = dados.tempo
+        })
 })
